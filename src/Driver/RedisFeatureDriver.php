6542d186f9f0fa32f602b919bd98c69209c407f9
Gabriel Moura <gmouradev96@gmail.com>
2024-08-23 16:01:27 -0300

    Updates dependencies

--- src/Driver/RedisFeatureDriver.php
+++ src/Driver/RedisFeatureDriver.php
@@ -12,37 +12,30 @@ use stdClass;
 
 class RedisFeatureDriver implements Driver
 {
-
     /**
      * The sentinel value for unknown features.
-     *
-     * @var stdClass
      */
     protected stdClass $unknownFeatureValue;
 
     /**
      * The prefix for the feature flags.
-     * @var string
      */
     private string $prefix = 'feature';
 
     /**
      * Create a new driver instance.
-     * @param RedisManager $redis
-     * @param Dispatcher $events
-     * @param array<string, (callable(mixed $scope): mixed)> $featureStateResolvers
+     *
+     * @param  array<string, (callable(mixed $scope): mixed)>  $featureStateResolvers
      */
     public function __construct(private readonly RedisManager $redis, private readonly Dispatcher $events, protected array $featureStateResolvers)
     {
         $this->unknownFeatureValue = new stdClass;
     }
 
-
     /**
      * Define an initial feature flag state resolver.
-     * @param string $feature
-     * @param (callable(mixed $scope): mixed) $resolver
-     * @return void
+     *
+     * @param  (callable(mixed $scope): mixed)  $resolver
      */
     public function define(string $feature, callable $resolver): void
     {
@@ -50,9 +43,9 @@ class RedisFeatureDriver implements Driver
         $this->featureStateResolvers[$feature] = $resolver;
     }
 
-
     /**
      * Define the names of all defined features.
+     *
      * @return array<string>
      */
     public function defined(): array
@@ -64,26 +57,22 @@ class RedisFeatureDriver implements Driver
     /**
      * Get multiple feature flag values.
      *
-     * @param array<string, array<int, mixed>> $features
+     * @param  array<string, array<int, mixed>>  $features
      * @return array<string, array<int, mixed>>
      */
     public function getAll(array $features): array
     {
 
         return Collection::make($features)
-            ->map(fn($scopes, $feature) => Collection::make($scopes)
-                ->map(fn($scope) => $this->get($feature, $scope))
+            ->map(fn ($scopes, $feature) => Collection::make($scopes)
+                ->map(fn ($scope) => $this->get($feature, $scope))
                 ->all())
             ->all();
 
     }
 
-
     /**
      * Retrieve a feature flag's value.
-     * @param string $feature
-     * @param mixed $scope
-     * @return mixed
      */
     public function get(string $feature, mixed $scope): mixed
     {
@@ -94,7 +83,6 @@ class RedisFeatureDriver implements Driver
         if ($result) {
             return $result;
         }
-
 
         return
             with($this->resolveValue($feature, $scope), function ($value) use ($feature, $scope) {
@@ -110,14 +98,11 @@ class RedisFeatureDriver implements Driver
 
     /**
      * Determine the initial value for a given feature and scope.
-     * @param string $feature
-     * @param mixed $scope
-     * @return mixed
      */
     protected function resolveValue(string $feature, mixed $scope): mixed
     {
 
-        if (!array_key_exists($feature, $this->featureStateResolvers)) {
+        if (! array_key_exists($feature, $this->featureStateResolvers)) {
             $this->events->dispatch(new UnknownFeatureResolved($feature, $scope));
 
             return $this->unknownFeatureValue;
@@ -126,13 +111,8 @@ class RedisFeatureDriver implements Driver
         return $this->featureStateResolvers[$feature]($scope);
     }
 
-
     /**
      * Set a feature flag's value.
-     * @param string $feature
-     * @param mixed $scope
-     * @param mixed $value
-     * @return void
      */
     public function set(string $feature, mixed $scope, mixed $value): void
     {
@@ -143,44 +123,33 @@ class RedisFeatureDriver implements Driver
         }
     }
 
-
     /**
      * Set a feature flag's value for all scopes.
-     * @param string $feature
-     * @param mixed $value
-     * @return void
      */
     public function setForAllScopes(string $feature, mixed $value): void
     {
         $this->redis->command('HMSET', ["$this->prefix:$feature", $value]);
     }
 
-
     /**
      * Delete a feature flag's value.
-     * @param string $feature
-     * @param mixed $scope
-     * @return void
      */
     public function delete(string $feature, mixed $scope): void
     {
         $this->redis->command('HDEL', ["$this->prefix:$feature", Feature::serializeScope($scope)]);
     }
 
-
     /**
      * Purge the given feature from storage.
-     * @param array|null $features
-     * @return void
      */
     public function purge(?array $features): void
     {
 
         if ($features === null) {
             $ds = $this->redis->command('KEYS', ["$this->prefix:*"]);
-            $ds = array_map(fn($d) => explode($this->prefix, $d)[1], $ds);
+            $ds = array_map(fn ($d) => explode($this->prefix, $d)[1], $ds);
 
-            $ds = array_map(fn($d) => $this->prefix . $d, $ds);
+            $ds = array_map(fn ($d) => $this->prefix.$d, $ds);
 
             foreach ($ds as $d) {
                 $this->redis->command('DEL', [$d]);
